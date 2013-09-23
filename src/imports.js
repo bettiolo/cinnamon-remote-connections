339e749b28c38905823400250fd3666efe885651
Marco Bettiolo <marco.bettiolo@7digital.com>
2013-09-23 16:36:03 +0100

    Reformatted code

--- src/imports.js
+++ src/imports.js
@@ -1,29 +1,35 @@
 "use strict";
 
-var imports = function() {
-    function Imports() { }
-    Imports.prototype = {
-        ui : ui,
-        gi : gi,
-	    misc : misc
-    };
-    return Imports;
+var imports = function () {
+	function Imports() {
+	}
+
+	Imports.prototype = {
+		ui : ui,
+		gi : gi,
+		misc : misc
+	};
+	return Imports;
 }();
 
 // Second level
 
-var ui = function() {
-    function Ui() { }
-    Ui.prototype = {
-        main : main,
-	    applet : applet,
-	    popupMenu : popupMenu
-    };
-    return Ui;
+var ui = function () {
+	function Ui() {
+	}
+
+	Ui.prototype = {
+		main : main,
+		applet : applet,
+		popupMenu : popupMenu
+	};
+	return Ui;
 }();
 
-var gi = function() {
-	function Gi() { }
+var gi = function () {
+	function Gi() {
+	}
+
 	Gi.prototype = {
 		St : St
 	};
@@ -32,24 +38,30 @@ var gi = function() {
 
 // Third level
 
-var main = function() {
-    function Main() { }
-    Main.prototype = {
+var main = function () {
+	function Main() {
+	}
 
-    };
-    return Main;
+	Main.prototype = {
+
+	};
+	return Main;
 }();
 
-var misc = function() {
-	function Misc() { }
+var misc = function () {
+	function Misc() {
+	}
+
 	Misc.prototype = {
 		util : util
 	};
 	return Main;
 }();
 
-var misc = function() {
-	function Misc() { }
+var misc = function () {
+	function Misc() {
+	}
+
 	Misc.prototype = {
 		util : util
 	};
@@ -58,18 +70,22 @@ var misc = function() {
 
 // Files in /usr/share/cinnamon/js
 
-var applet = function() {
-    function Applet() { }
-    Applet.prototype = {
-        IconApplet : IconApplet,
-	    AppletPopupMenu : AppletPopupMenu,
-	    MenuItem : MenuItem
-    };
-    return Applet;
+var applet = function () {
+	function Applet() {
+	}
+
+	Applet.prototype = {
+		IconApplet : IconApplet,
+		AppletPopupMenu : AppletPopupMenu,
+		MenuItem : MenuItem
+	};
+	return Applet;
 }();
 
-var popupMenu = function() {
-	function PopupMenu() { }
+var popupMenu = function () {
+	function PopupMenu() {
+	}
+
 	PopupMenu.prototype = {
 		PopupMenuManager : PopupMenuManager,
 		PopupMenuSection : PopupMenuSection,
@@ -78,8 +94,10 @@ var popupMenu = function() {
 	return PopupMenu;
 }();
 
-var settings = function() {
-	function Settings() { }
+var settings = function () {
+	function Settings() {
+	}
+
 	Settings.prototype = {
 		AppletSettings : AppletSettings,
 		BindingDirection : BindingDirection
@@ -91,10 +109,13 @@ var settings = function() {
 
 // Files in /usr/share/cinnamon/misc
 
-var util = function() {
-	function Util() { }
+var util = function () {
+	function Util() {
+	}
+
 	Util.prototype = {
-		spawnCommandLine : function (commandLine) {}
+		spawnCommandLine : function (commandLine) {
+		}
 	};
 	return Util;
 }();
@@ -103,8 +124,10 @@ var util = function() {
 
 // External
 
-var St = function() {
-	function St() { }
+var St = function () {
+	function St() {
+	}
+
 	St.prototype = {
 	};
 	return St;
